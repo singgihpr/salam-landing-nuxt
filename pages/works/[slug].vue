122040a75932aeb8dc444be033ea0ed42df01e4d
singgihpr <singgih.hans@gmail.com>
2024-08-21 22:35:20 +0700

    Feat: add back button on works

--- pages/works/[slug].vue
+++ pages/works/[slug].vue
@@ -25,13 +25,11 @@
                 class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
                 <div class="lg:pr-4">
                     <div class="lg:max-w-lg">
-                        <p class="text-base font-semibold leading-7 text-indigo-600">Product 1</p>
+                        <p class="text-base font-semibold leading-7 text-indigo-600">{{ works.date }}</p>
                         <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                             {{ slug }}
                         </h1>
-                        <p class="mt-6 text-xl leading-8 text-gray-700">Aliquet nec orci mattis amet quisque ullamcorper
-                            neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae
-                            feugiat egestas.</p>
+                        <p class="mt-6 text-xl leading-8 text-gray-700">{{ works.description }}</p>
                     </div>
                 </div>
             </div>
@@ -39,6 +37,12 @@
                 class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
                 <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
                     src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png" alt="" />
+                <div class="flex w-full justify-end mt-5">
+                    <button @click="goBack" class="flex items-center">
+                        <ArrowLongLeftIcon class="w-5 text-indigo-600" aria-hidden="true" />
+                        <span><strong class="ml-3 text-indigo-600 font-semibold text-gray-900">Back</strong></span>
+                    </button>
+                </div>
             </div>
             <div
                 class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
@@ -71,13 +75,8 @@
                         <p class="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id
                             blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris
                             varius vulputate et ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac
-                            lorem vel integer orci.</p>
-                        <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
-                        <p class="mt-6">Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in.
-                            Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat
-                            vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim
-                            cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
-                            diam.</p>
+                            lorem vel integer orci.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -86,9 +85,18 @@
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/vue/20/solid'
+import { useRoute, useRouter } from 'vue-router';
+import { ArrowLongLeftIcon, CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/vue/20/solid'
+import { useWorksStore } from '~/stores/works';
+
 
 const route = useRoute();
+const router = useRouter();
 const slug = route.params.slug;
+const worksStore = useWorksStore();
+const works = worksStore.works.find(works => works.href === `/works/${slug}`);
+
+function goBack() {
+  router.back(); // Navigate back to the previous page
+}
 </script>
